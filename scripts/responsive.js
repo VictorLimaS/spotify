93af867c0a25d386cfa6c2c8bc7cf6d8833d11c3
Victor Lima <victorlimasantos99@hotmail.com>
2024-01-31 06:35:50 -0300

    Update responsive.js
    Devido a mudança do html, terei que alterar toda responsividade

--- scripts/responsive.js
+++
@@ -1,7 +0,0 @@
-const hamburguer = document.querySelector("")
-const navmenu = document.querySelector("")
-
-hamburguer.addEventListener("click", () => {
-  hamburguer.classList.toggle("active")
-  navmenu.classList.toggle("active")
-})
