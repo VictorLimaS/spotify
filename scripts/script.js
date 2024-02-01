4f27cc788eabc6d376cc0da7743210048665113b
Victor Lima <victorlimasantos99@hotmail.com>
2024-01-31 21:20:59 -0300

    script de busca
    script de busca de cantor

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,31 @@
-const searchInput = document.getElementById("search-input")
-const resultsArtist = document.getElementById("result-artist")
-const resultPlaylist = document.getElementById("cards-container")
-
-function requestApi(searchTerm) {
-  const url = `https://api-spotify-eta.vercel.app/artists?name_like=${searchTerm}`
+// Função para fazer a requisição da API
+function requestApi(termoDeBusca) {
+  // URL do endpoint da API
+  const url = `https://api-spotify-eta.vercel.app/artists?name_like=${termoDeBusca}`;
+  
+  // Buscar dados da API usando a função fetch
   fetch(url)
-    .then((response) => response.json())
-    .then((result) => displayResults(result))
+    // Quando a resposta da requisição estiver disponível, converte a resposta para JSON
+    .then((resposta) => resposta.json())
+    // Quando a conversão para JSON estiver completa, chama a função para exibir os resultados
+    .then((resultado) => exibirResultados(resultado));
 }
-function displayResults(result) {
-  const artistName = document.getElementById("artist-name")
-  const artistImage = document.getElementById("artist-img")
-  resultPlaylist.style.display = "none"
 
-  result.forEach((element) => {
-    artistName.innerText = element.name
-    artistImage.src = element.urlImg
-  })
+// Função para exibir os resultados da busca
+function exibirResultados(resultado) {
+  // Obter elementos individuais
+  const nomeArtista = document.getElementById("artist-name");
+  const imagemArtista = document.getElementById("artist-img");
 
-  resultsArtist.classList.remove("hidden")
+  // Esconder resultados da playlist
+  resultPlaylist.style.display = "none";
+
+  // Exibir nome e imagem de cada artista
+  resultado.forEach((elemento) => {
+    nomeArtista.innerText = elemento.name;
+    imagemArtista.src = elemento.urlImg;
+  });
+
+  // Mostrar o container de resultados de artistas
+  resultsArtist.classList.remove("hidden");
 }
-document.addEventListener("input", function () {
-  const searchTerm = searchInput.value.toLowerCase()
-  if (searchTerm === "") {
-    resultPlaylist.style.display = "flex"
-    resultsArtist.style.display = "none"
-    return
-  }
-  resultPlaylist.style.display = "none"
-  resultsArtist.style.display = "flex"
-
-  requestApi(searchTerm)
-})
